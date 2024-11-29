4a3b25321c18a9fc722edd09f2dc9eb8aae2a934
AFCMS <afcm.contact@gmail.com>
2024-11-29 01:28:33 +0100

    Basic comments system

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,16 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Post;
+use App\Form\CommentType;
+use App\Repository\CommentRepository;
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class PostController extends AbstractController
 {
     #[Route('/post/{id}', name: 'app_post')]
-    public function index(int $id, EntityManagerInterface $entityManager): Response
+    public function index(Request $request, int $id, EntityManagerInterface $entityManager): Response
     {
         $post = $entityManager->getRepository(Post::class)->find($id);
 
@@ -19,8 +24,65 @@ class PostController extends AbstractController
             throw $this->createNotFoundException('The post does not exist');
         }
 
+        $comment = new Comment();
+
+        /** @var CommentRepository $rep */
+        $is_update = false;
+        $rep = $entityManager->getRepository(Comment::class);
+        $old_post = $rep->findByUserAndPost($this->getUser()->getId(), $post->getId());
+
+        if ($old_post) {
+            $comment = $old_post;
+            $is_update = true;
+        } else {
+            $comment->setUser($this->getUser());
+            $comment->setPost($post);
+            $comment->setCreatedAt(new DateTime());
+        }
+
+        $form_comment = $this->createForm(CommentType::class, $comment);
+
+        $form_comment->handleRequest($request);
+
+        if ($form_comment->isSubmitted() && $form_comment->isValid()) {
+            /** @var Comment $cat */
+            $c = $form_comment->getData();
+
+            $entityManager->persist($c);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_post', ['id' => $id]);
+        }
+
         return $this->render('post/index.html.twig', [
             'post' => $post,
+            'form_comment' => $form_comment,
+            'is_update' => $is_update,
         ]);
+    }
+
+    #[Route('/post/{id}/delcomment', name: 'app_post_delete_comment')]
+    public function delcomment(Request $request, int $id, EntityManagerInterface $entityManager): Response
+    {
+        $post = $entityManager->getRepository(Post::class)->find($id);
+
+        if (!$post) {
+            throw $this->createNotFoundException('The post does not exist');
+        }
+
+        $user = $this->getUser();
+
+        /** @var CommentRepository $rep */
+        $rep = $entityManager->getRepository(Comment::class);
+
+        $comment = $rep->findByUserAndPost($user->getId(), $post->getId());
+
+        if (!$comment) {
+            throw $this->createNotFoundException('The comment does not exist');
+        }
+
+        $entityManager->remove($comment);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_post', ['id' => $id]);
     }
 }
--- src/Repository/CommentRepository.php
+++ src/Repository/CommentRepository.php
@@ -15,29 +15,18 @@ class CommentRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Comment::class);
     }
-
-//    /**
-//     * @return Comment[] Returns an array of Comment objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('c')
-//            ->andWhere('c.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('c.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Comment
-//    {
-//        return $this->createQueryBuilder('c')
-//            ->andWhere('c.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    
+    /**
+     * @return Comment|null
+     */
+    public function findByUserAndPost(int $userId, int $postId)
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.user = :userId')
+            ->andWhere('c.post = :postId')
+            ->setParameter('userId', $userId)
+            ->setParameter('postId', $postId)
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
 }
