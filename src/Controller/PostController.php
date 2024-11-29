5e7bd93c8bafcc969790a1829122f10a398e6d5f
AFCMS <afcm.contact@gmail.com>
2024-11-29 03:27:21 +0100

    Allow admin to delete comments

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -101,4 +101,35 @@ class PostController extends AbstractController
 
         return $this->redirectToRoute('app_post', ['id' => $id]);
     }
+
+    #[Route('/post/{post_id}/delcomment/{id}', name: 'app_post_delete_comment_admin')]
+    public function delcommentadmin(Request $request, int $post_id, int $id, EntityManagerInterface $entityManager): Response
+    {
+        $post = $entityManager->getRepository(Post::class)->find($post_id);
+
+        if (!$post) {
+            throw $this->createNotFoundException('The post does not exist');
+        }
+
+        /** @var User|null $user */
+        $user = $this->getUser();
+
+        if (!$user) {
+            throw $this->createNotFoundException('The user does not exist or isn\'t connected');
+        }
+
+        /** @var CommentRepository $rep */
+        $rep = $entityManager->getRepository(Comment::class);
+
+        $comment = $rep->find($id);
+
+        if (!$comment) {
+            throw $this->createNotFoundException('The comment does not exist');
+        }
+
+        $entityManager->remove($comment);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_post', ['id' => $post_id]);
+    }
 }
