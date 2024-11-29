281e6d53fa8584f4ec61c353a7da92ab5da3ff90
AFCMS <afcm.contact@gmail.com>
2024-11-29 01:38:51 +0100

    Fix rendering of posts page

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -5,6 +5,7 @@ namespace App\Twig;
 use App\Entity\Category;
 use Doctrine\ORM\EntityManagerInterface;
 use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
 use Twig\TwigFunction;
 
 class AppExtension extends AbstractExtension
@@ -23,11 +24,26 @@ class AppExtension extends AbstractExtension
         ];
     }
 
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('firstWords', [$this, 'getFirstWords']),
+        ];
+    }
+
     /**
      * @return Category[]
      */
     public function getCategories(): array
     {
         return $this->entityManager->getRepository(Category::class)->findAll();
+    }
+
+    public function getFirstWords(string $content, int $words = 20): string
+    {
+        $content = explode(' ', strip_tags($content));
+        $wordsArray = array_slice($content, 0, $words);
+
+        return implode(' ', $wordsArray) . (count($content) > $words ? '...' : '');
     }
 }
